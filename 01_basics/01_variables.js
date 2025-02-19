bc57ef824719095e7ce3a44a159e8f56a4c2394a
Harish mahur <102143039+Harry0809@users.noreply.github.com>
2025-02-19 16:52:53 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const Name = "Harish";
+let SurName = "Mahur";
+var eMail = "hmahur20@gmail.com";
+state = "Delhi";
+ let city;
+
+
+SurName = "Harry";
+eMail = "fghjk@ghbj.com";
+state = "Jaipur";
+
+console.table([Name, SurName, eMail, state, city]);
+
+
