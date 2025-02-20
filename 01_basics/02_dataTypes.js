66bc7202f6f4d5c9d46f7d818ab6845537ab9385
Harish mahur <102143039+Harry0809@users.noreply.github.com>
2025-02-20 16:03:36 +0000

    DataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use Strict"; //treat all JS code as newer version
+
+// alert(3+3)  // we are using nodeJS not, not browser
+
+let name = "Harish";
+let age = 25;
+let isLoggedIn = false;
+
+//number 
+//bigint
+//string
+//boolean
+//undefined
+
+console.log(typeof "Harish"); //string
+console.log(typeof null); //object
+console.log(typeof undefined);  //undefined
