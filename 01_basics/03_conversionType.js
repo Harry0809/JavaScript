da803a02b4df053b332afb7097a72e87b2c1a0b9
Harish mahur <102143039+Harry0809@users.noreply.github.com>
2025-02-20 16:23:15 +0000

    Conversion Of Data Types

---
+++ 01_basics/03_conversionType.js
@@ -0,0 +1,37 @@
+let age = "asa25";
+
+console.log(typeof age);
+console.log(typeof(age));
+
+let ageInNumber = Number(age);
+console.log(typeof ageInNumber);
+
+console.log(ageInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false =>0
+
+
+let isLoggedIn = 1;
+
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(booleanLoggedIn);
+
+
+//  1 => true, 0 => false
+//  "Harish" => true
+//  "" => false
+
+
+let number = 25;
+
+let stringNumber = String(number);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
